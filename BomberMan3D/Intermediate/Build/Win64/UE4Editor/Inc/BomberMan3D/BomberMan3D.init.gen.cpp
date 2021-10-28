b0f7c661d8c0e24dd82aec10aa92f34e83a00fba
Damien McAlear <damien-mca@hotmail.com>
2021-10-28 16:08:51 +0100

    Create Player Character
    Created PlayerCharacter class.

--- BomberMan3D/Intermediate/Build/Win64/UE4Editor/Inc/BomberMan3D/BomberMan3D.init.gen.cpp
+++ BomberMan3D/Intermediate/Build/Win64/UE4Editor/Inc/BomberMan3D/BomberMan3D.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeBomberMan3D_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x95FB2A59,
-				0xE48A51D3,
+				0xAE4B4903,
+				0xB87B75D3,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
---
+++ BomberMan3D/Intermediate/Build/Win64/UE4Editor/Inc/BomberMan3D/PlayerCharacter.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BOMBERMAN3D_PlayerCharacter_generated_h
+#error "PlayerCharacter.generated.h already included, missing '#pragma once' in PlayerCharacter.h"
+#endif
+#define BOMBERMAN3D_PlayerCharacter_generated_h
+
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_SPARSE_DATA
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_RPC_WRAPPERS
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPlayerCharacter(); \
+	friend struct Z_Construct_UClass_APlayerCharacter_Statics; \
+public: \
+	DECLARE_CLASS(APlayerCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/BomberMan3D"), NO_API) \
+	DECLARE_SERIALIZER(APlayerCharacter)
+
+
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAPlayerCharacter(); \
+	friend struct Z_Construct_UClass_APlayerCharacter_Statics; \
+public: \
+	DECLARE_CLASS(APlayerCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/BomberMan3D"), NO_API) \
+	DECLARE_SERIALIZER(APlayerCharacter)
+
+
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APlayerCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APlayerCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APlayerCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APlayerCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APlayerCharacter(APlayerCharacter&&); \
+	NO_API APlayerCharacter(const APlayerCharacter&); \
+public:
+
+
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APlayerCharacter(APlayerCharacter&&); \
+	NO_API APlayerCharacter(const APlayerCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APlayerCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APlayerCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(APlayerCharacter)
+
+
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_PRIVATE_PROPERTY_OFFSET
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_9_PROLOG
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_SPARSE_DATA \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_RPC_WRAPPERS \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_INCLASS \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_SPARSE_DATA \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	BomberMan3D_Source_BomberMan3D_PlayerCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BOMBERMAN3D_API UClass* StaticClass<class APlayerCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BomberMan3D_Source_BomberMan3D_PlayerCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
