f864bf9282486285b8bca0c4ea49fd44eedd5ded
Damien McAlear <damien-mca@hotmail.com>
2021-10-30 00:00:21 +0100

    Win Conditions
    Added checks for player deaths.
    Added placeholder checks for win and draw conditions.
    ToDo: Add score increases for wins.

--- BomberMan3D/Source/BomberMan3D/BomberMan3DGameModeBase.cpp
+++ BomberMan3D/Source/BomberMan3D/BomberMan3DGameModeBase.cpp
@@ -6,9 +6,13 @@
 #include "Engine/World.h"
 #include "PlayerCharacter.h"
 #include "BomberMan3DGameStateBase.h"
+#include "Bomb.h"
 
 ABomberMan3DGameModeBase::ABomberMan3DGameModeBase()
 {
+	PrimaryActorTick.bStartWithTickEnabled = true;
+	PrimaryActorTick.bCanEverTick = true;
+
 	// Set game state class to our Blueprinted state
 	static ConstructorHelpers::FClassFinder<ABomberMan3DGameStateBase> gameStateBPClass(TEXT("/Game/Blueprints/GameModes/BPBomberMan3DGameStateBase"));
 	if (gameStateBPClass.Class != nullptr)
@@ -89,8 +93,50 @@ void ABomberMan3DGameModeBase::CheckPlayers()
 {
 	// ToDo: Check which players are alive
 
-	// Get game state
-	GetGameState<ABomberMan3DGameStateBase>();
+	// Get all players
+	TArray<AActor*> foundPlayers;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), DefaultPawnClass, foundPlayers);
+
+	TArray<APlayerCharacter*> livingPlayers;
+
+	for (int i = 0; i < foundPlayers.Num(); i++)
+	{
+		APlayerCharacter* player = dynamic_cast<APlayerCharacter*>(foundPlayers[i]);
+
+		if (!player->isAlive())
+		{
+			// Stop movement to player
+			player->DisableInput(dynamic_cast<APlayerController*>(player->GetController()));
+		}
+		else
+		{
+			livingPlayers.Add(player);
+		}
+	}
+
+	if (livingPlayers.Num() == 0)
+	{
+		// Draw
+	}
+	else if (livingPlayers.Num() == 1)
+	{
+		// 1 player alive, need to see if they'll die "soon"
+
+		// Check for active explosions
+		// Get all explosions
+		TArray<AActor*> foundExplosions;
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), ABomb::StaticClass(), foundExplosions);
+
+		if (foundExplosions.Num() == 0)
+		{
+			// No explosions, living player wins
+			// Increase score of living player in game state
+			//livingPlayers[0];
+
+			// Get game state
+			//GetGameState<ABomberMan3DGameStateBase>();
+		}
+	}	
 }
 
 void ABomberMan3DGameModeBase::ResetLevel()
